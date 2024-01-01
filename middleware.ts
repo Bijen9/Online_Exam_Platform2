d329682f8d1787fe5b20e079dcce47c4e7fbe99b
diwasdhungana <diwasdhungana@gmail.com>
2024-01-02 00:48:20 +0545

    try 8

--- middleware.ts
+++ middleware.ts
@@ -1,41 +1,41 @@
-// import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
-// // import { redirect } from "next/dist/server/api-utils";
-// // import { NextResponse } from "next/server";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/dist/server/api-utils";
+import { NextResponse } from "next/server";
 
-// export default authMiddleware({
-//   publicRoutes: [
-//     "/",
-//     "/sign-in/[[...index]]",
-//     "/sign-up/[[...index]]",
-//     "/api/webhook/clerk",
-//   ],
-//   ignoredRoutes: ["/api/webhook/clerk"],
-//   // clerk settings
-//   afterAuth(auth, req, res) {
-//     // handle users who aren't authenticated
-
-//     if (!auth.userId && !auth.isPublicRoute) {
-//       // console.log("redirecting to sign in");
-//       // console.log(auth.userId, auth.isPublicRoute);
-//       return redirectToSignIn({ returnBackUrl: req.url });
-//     }
-//     if (auth.userId && req.nextUrl.pathname == "/sign-in") {
-//       const bufferuser = new URL("/bufferuser", req.url);
-//       return NextResponse.redirect(bufferuser.href);
-//     }
-//   },
-// });
-
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
-
-import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
-  ignoredRoutes: ["/((?!api|trpc))(_next|.+..+)(.*)"],
-  publicRoutes: ["/api/webhooks/clerk"],
+  publicRoutes: [
+    "/",
+    "/sign-in/[[...index]]",
+    "/sign-up/[[...index]]",
+    "/api/webhook/clerk",
+  ],
+  ignoredRoutes: ["/api/webhook/clerk"],
+  // clerk settings
+  afterAuth(auth, req, res) {
+    // handle users who aren't authenticated
+
+    if (!auth.userId && !auth.isPublicRoute) {
+      console.log("redirecting to sign in");
+      console.log(auth.userId, auth.isPublicRoute);
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+    if (auth.userId && req.nextUrl.pathname == "/sign-in") {
+      const bufferuser = new URL("/bufferuser", req.url);
+      return NextResponse.redirect(bufferuser.href);
+    }
+  },
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+
+// import { authMiddleware } from "@clerk/nextjs";
+// export default authMiddleware({
+//   ignoredRoutes: ["/((?!api|trpc))(_next|.+..+)(.*)"],
+//   publicRoutes: ["/api/webhooks/clerk"],
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
