5cdd86a52bc3a43e3c59d739a2f710fd897d488f
diwasdhungana <diwasdhungana@gmail.com>
2024-01-01 19:21:27 +0545

    Revert "test push 3"
    This reverts commit fecba15229d62dee177b96f16f33abad2efad05a.

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,6 +4,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="flex min-h-screen w-full items-center justify-center">
       {children}
+      {/* {children} */}
     </main>
   );
 };
