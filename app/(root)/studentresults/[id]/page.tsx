10281bdaa0ff851662c2c9315f3b830a07037321
diwasdhungana <diwasdhungana@gmail.com>
2024-01-04 11:31:25 +0545

    last commit with result

---
+++ app/(root)/studentresults/[id]/page.tsx
@@ -0,0 +1,75 @@
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import TestCard from "@/components/cards/TestCard";
+import { getEditTest, getTestById } from "@/lib/actions/test.action";
+import { getUserId } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { getResultMarks } from "@/lib/actions/result.action";
+
+const ResultDetai = async ({ params, searchParams }) => {
+  const { id } = params;
+  const test = await getTestById({ testId: id });
+  const clerkId: string = auth().userId!;
+
+  const userId = await getUserId({ clerkId });
+  const tests: any = await getEditTest({ userId });
+  const results = await getResultMarks({
+    userId,
+    testId: id,
+  });
+  console.log(results);
+  return (
+    <div>
+      <div className="mt-10 flex w-full flex-col justify-between gap-6 sm:flex-column sm:item-center">
+        <h1 className="h1-bold text-dark100_light900">Results</h1>
+        {results && results.status == "success" ? (
+          <div>
+            <div className="flex flex-row justify-between">
+              <h1 className="h1-bold text-dark100_light900">Results</h1>
+              <h1 className="h1-bold text-dark100_light900">
+                Total Marks: {results.totalMarks}
+              </h1>
+            </div>
+            {results.correctMCQAnswers!.map((result: any) => (
+              <div className="flex flex-row justify-between">
+                <h1 className="h1-bold text-dark100_light900">
+                  {result.question}
+                </h1>
+                <h1 className="h1-bold text-dark100_light900">
+                  Marks: {result.marks}
+                </h1>
+              </div>
+            ))}
+
+            {results.correctTFAnswers!.map((result: any) => (
+              <div className="flex flex-row justify-between">
+                <h1 className="h1-bold text-dark100_light900">
+                  {result.question}
+                </h1>
+                <h1 className="h1-bold text-dark100_light900">
+                  Marks: {result.marks}
+                </h1>
+              </div>
+            ))}
+
+            {results.correctWrittenAnswers!.map((result: any) => (
+              <div className="flex flex-row justify-between">
+                <h1 className="h1-bold text-dark100_light900">
+                  {result.question}
+                </h1>
+                <h1 className="h1-bold text-dark100_light900">
+                  Marks: {result.marks}
+                </h1>
+              </div>
+            ))}
+          </div>
+        ) : (
+          "No results published for this test yet."
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ResultDetai;
---
+++ app/(root)/studentresults/page.tsx
@@ -0,0 +1,36 @@
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import TestCard from "@/components/cards/TestCard";
+import { getEditTest } from "@/lib/actions/test.action";
+import { getUserId } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+
+export default async function Result() {
+  const clerkId: string = auth().userId!;
+
+  const userId = await getUserId({ clerkId });
+  const tests: any = await getEditTest({ userId });
+
+  return (
+    <>
+      <div className="mt-10 flex w-full flex-col justify-between gap-6 sm:flex-column sm:item-center">
+        <h1 className="h1-bold text-dark100_light900">Results</h1>
+        {tests!.map((test: any) => (
+          <Link href={`/studentresults/${test._id}`}>
+            <TestCard
+              key={test._id}
+              name={test.name}
+              description={test.description}
+              startTime={test.startTime}
+              endTime={test.endTime}
+              status={test.status}
+              totalQuestion={test.totalQuestions}
+              createdAt={test.createdAt}
+            />
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+}
